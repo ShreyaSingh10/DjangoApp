5989261f8e5eb8a11e3fb534b408b0f388ba8e8a
ShreyaSingh10 <shreyasssingh10@gmail.com>
2018-03-30 18:08:57 +0530

    Third Commit - passing dynamic data into templates

--- djangoproject/posts/views.py
+++ djangoproject/posts/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
 	#return HttpResponse('Hey der m posting')
-	return render(request, 'posts/index.html') 
+	return render(request, 'posts/index.html', {'title' : 'Latest Posts'
+		}) 
